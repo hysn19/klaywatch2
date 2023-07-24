7919345bd86ca6e9dc0436047e1d65c6d0fba731
김준철 <47551453+hysn19@users.noreply.github.com>
2023-07-24 21:32:28 +0900

    Update task.service.ts

--- src/task/task.service.ts
+++ src/task/task.service.ts
@@ -1,104 +1,103 @@
-import { Injectable } from '@nestjs/common';
-import { Cron } from '@nestjs/schedule';
-
+import { Injectable, HttpService } from '@nestjs/common';
+import { Cron, CronExpression } from '@nestjs/schedule';
 import { SlackService } from '../slack/slack.service';
 import { CaverService } from '../klaytn/caver/caver.service';
 import { SlackMessage } from '../slack/slack.message';
 import { logger } from 'config/winston';
+import { CronJob } from './custom.decorator';
 
 @Injectable()
 export class TaskService {
-  private slackService = new SlackService(
-    'https://hooks.slack.com/services/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',
-  );
-  private caverService = new CaverService(
-    'https://public-node-api.klaytnapi.com/v1/cypress',
-  );
-
   private isCronJobActive = false;
+  private readonly tasks: Task[];
 
-  private async getAccountByCaverAndSendSlack(
-    accountAddress: string,
-    accountName: string,
+  constructor(
+    private readonly slackService: SlackService,
+    private readonly caverService: CaverService,
+    private readonly httpService: HttpService, // Assuming you need to use HttpService for API calls
   ) {
-		try {
-			const [currentBalance, transactionCount, currentBlockNumber] =
-      await Promise.all([
-        this.caverService.getBalance(accountAddress),
-        this.caverService.getTransactionCount(accountAddress),
-        this.caverService.getCurrentBlockNumber(),
-      ]);
-			
-			logger.info(`balance: ${currentBalance}`);
-			logger.info(`transactionCount: ${transactionCount}`);
-			logger.info(`currentBlockNumber: ${currentBlockNumber}`);
-
-			const message = new SlackMessage(
-				accountAddress,
-   			accountName,
-				currentBalance,
-				transactionCount,
-				currentBlockNumber,
-				this.isAnomalyDetected(accountAddress, currentBalance, transactionCount),
-			).payload;
-
-			await this.slackService.sendMessage(message);
-		} catch (error) {
-			logger.error('Error occurred while processing: ${error}')
-		}
-  }
-
-  @Cron('0 0 * * * *') // runs every hour at the 0 minute.
-  async handleCron1() {
-    this.isCronJobActive = true;
-
-    const accountAddress = '0x2fd3ff6e4ead7430ea25bab5e5b2b073492b7e6e';
-    const accountName = 'Reward: Kakao Pay 3';
-
-    await this.getAccountByCaverAndSendSlack(accountAddress, accountName);
-  }
-
-  @Cron('0 0/10 * * * *') // run every 10 minutes
-  async handleCron2() {
-    this.isCronJobActive = true;
-
-    const accountAddress = '0x1f55eadcc398e9a2d3b8b505c993e19d210786bf';
-    const accountName = 'Reward: Kakao Pay';
-
-    await this.getAccountByCaverAndSendSlack(accountAddress, accountName);
-  }
-
-  @Cron('0 0 0 * * *') // run every midnight
-  async handleCron3() {
-    this.isCronJobActive = true;
-
-    const array = [
-      {
-        accountAddress: '0x2888d9dcbd6cefb92a76712eca61391c37df8122',
-        accountName: 'StakingV1 Account',
-      },
-      {
-        accountAddress: '0x4Ff8B6c558cBA6b20c962F301908D1D70cEEEA7c',
-        accountName: 'StakingV2 Account',
-      },
-      {
-        accountAddress: '0xbe02aba56bae1624e2c4f029e3a79308e2a19e98',
-        accountName: 'AuthFee Account',
-      },
-      {
-        accountAddress: '0x020794caf06273ef3a9b4b0323c72341ec070b15',
-        accountName: 'Reward1 Account',
-      },
+    this.tasks = [
+      new Task(
+        '0x2fd3ff6e4ead7430ea25bab5e5b2b073492b7e6e',
+        'Reward: Kakao Pay 3',
+      ),
+      new Task(
+        '0x1f55eadcc398e9a2d3b8b505c993e19d210786bf',
+        'Reward: Kakao Pay',
+      ),
+      new Task(
+        '0x2888d9dcbd6cefb92a76712eca61391c37df8122',
+        'StakingV1 Account',
+      ),
+      new Task(
+        '0x4Ff8B6c558cBA6b20c962F301908D1D70cEEEA7c',
+        'StakingV2 Account',
+      ),
+      new Task('0xbe02aba56bae1624e2c4f029e3a79308e2a19e98', 'AuthFee Account'),
+      new Task('0x020794caf06273ef3a9b4b0323c72341ec070b15', 'Reward1 Account'),
     ];
+  }
+
+  private async getAccountByCaverAndSendSlack(task: Task) {
+    try {
+      const [currentBalance, transactionCount, currentBlockNumber] =
+        await Promise.all([
+          this.caverService.getBalance(task.accountAddress),
+          this.caverService.getTransactionCount(task.accountAddress),
+          this.caverService.getCurrentBlockNumber(),
+        ]);
+
+      logger.info(`balance: ${currentBalance}`);
+      logger.info(`transactionCount: ${transactionCount}`);
+      logger.info(`currentBlockNumber: ${currentBlockNumber}`);
+
+      const message = new SlackMessage(
+        task.accountAddress,
+        task.accountName,
+        currentBalance,
+        transactionCount,
+        currentBlockNumber,
+        this.isAnomalyDetected(
+          task.accountAddress,
+          currentBalance,
+          transactionCount,
+        ),
+      ).payload;
+
+      await this.slackService.sendMessage(message);
+    } catch (error) {
+      logger.error(`Error occurred while processing ${task.accountName}: ${error}`);
+    }
+  }
+
+  @CronJob(CronExpression.EVERY_HOUR)
+  handleCron1() {
+    this.isCronJobActive = true;
+    this.processTasks(0);
+  }
+
+  @CronJob(CronExpression.EVERY_10_MINUTES)
+  handleCron2() {
+    this.isCronJobActive = true;
+    this.processTasks(1);
+  }
+
+  @CronJob(CronExpression.EVERY_DAY_AT_MIDNIGHT)
+  handleCron3() {
+    this.isCronJobActive = true;
+    this.processTasks(2, 5);
+  }
+
+  private async processTasks(startIndex: number, endIndex?: number) {
+    const tasksToProcess = endIndex
+      ? this.tasks.slice(startIndex, endIndex + 1)
+      : [this.tasks[startIndex]];
 
     await Promise.all(
-      array.map(async (account) => {
-        await this.getAccountByCaverAndSendSlack(
-          account.accountAddress,
-          account.accountName,
-        );
-      }),
+      tasksToProcess.map((task) => this.getAccountByCaverAndSendSlack(task)),
     );
+
+    this.isCronJobActive = false;
   }
 
   isAnomalyDetected(
@@ -115,4 +114,11 @@ export class TaskService {
   isCronJobRunning(): boolean {
     return this.isCronJobActive;
   }
+}
+
+class Task {
+  constructor(
+    public readonly accountAddress: string,
+    public readonly accountName: string,
+  ) {}
 }
